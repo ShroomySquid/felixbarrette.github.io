8430de0580b4fb850dacbf803d5db750b5bf88af
felix <f.a.r.barrette@hotmail.com>
2024-09-07 14:52:28 -0400

    Removed useless files, cleaned features I deemd ugly, add project images, add link to github and linkedin, added background images

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const projects = [
-  {name: "Minishell", git_url:"https://github.com/ShroomySquid/Minishell", image: "", description: "Interact with your computer, send commands the same way a traditionnal shell would. Made using C."},
-  {name: "FT_IRC", git_url:"https://github.com/ShroomySquid/IRC", image: "", description: "Following standard IRC protocols, join server, chat with your friends or with strangers. Made using C++."},
+  {name: "Minishell", git_url:"https://github.com/ShroomySquid/Minishell", image: "img/minishell.png", description: "Interact with your computer, send commands the same way a traditionnal shell would. Made using C."},
+  {name: "FT_IRC", git_url:"https://github.com/ShroomySquid/IRC", image: "img/irc.png", description: "Following standard IRC protocols, join server, chat with your friends or with strangers. Made using C++."},
   {name: "Cube3D", git_url:"https://github.com/ShroomySquid/Cub3D", image: "", description: "Travel and explore in this raycasting game. Made using C and the Minilibx graphic library (similar to OpenGL)."},
-  {name: "Quizz Game", git_url:"https://github.com/ShroomySquid/QuizzGame", image: "", description: "Test your general knowldege with this quizz app! Made using React, HTML, CSS and javascript."},
-  {name: "Movie Watchlist", git_url:"https://github.com/ShroomySquid/Movie-Watchlist", image: "", description: "Search for your favorite movie and save it in the watchlist. Made using HTML, CSS and javascript."},
+  {name: "Quizz Game", git_url:"https://github.com/ShroomySquid/QuizzGame", image: "img/quizz.png", description: "Test your general knowldege with this quizz app! Made using React, HTML, CSS and javascript."},
+  {name: "Movie Watchlist", git_url:"https://github.com/ShroomySquid/Movie-Watchlist", image: "img/watchlist.png", description: "Search for your favorite movie and save it in the watchlist. Made using HTML, CSS and javascript."},
 ];
 
 const project_box = document.getElementById("project_container");
@@ -13,8 +13,8 @@ const git_link = document.getElementsByClassName("git_link");
 for (let i = 0; i < projects.length; i++)  {
   let project_html = `
     <div class="project">
-      <h2>${projects[i].name}</h2>
-      <img src="img/profile_pic.jpg" alt="Project Image" width=150 heigth=150>
+      <h2 class="project_name">${projects[i].name}</h2>
+      <img src=${projects[i].image} alt="Project Image" class="project_image" width=150 heigth=150>
       <p class="project_description">${projects[i].description}</p>
       <a href=${projects[i].git_url} target="_blank" class="git_link">Github Page</a>
     </div>
